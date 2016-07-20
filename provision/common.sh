2a05b01b93822d67ff8b08d08ea5a51ebf7371dc
Blake Beus <BlakeBeus@users.noreply.github.com>
2016-07-20 13:31:01 -0600

    Initial Commit

---
+++ provision/common.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+#THIS SCRIPT IS FOR SETTING UP LOCAL VAGRANT BOXES. 
+#IT IS NOT SECURE ENOUGH FOR PRODUCTION.
+
+#set some variables
+
+#site URL - THIS MUST MATCH WHAT IS IN THE VAGRANTFILE
+URL='dev.example.com'
+
+#create MySQL DB and User
+#assumes mysql_secure_install hasn't been run yet
+DBNAME='example_database'
+USER='example_user'
+PASSWORD='password1234'
+
+
+#Update
+sudo apt-get -y update
+
+# install stuff
+sudo apt-get install -y nginx
+sudo apt-get install -y mariadb-server
+sudo apt-get install -y php-fpm php-mysql php-cli
+sudo apt-get install -y curl git
+
+
+#create conf file for website
+cat <<EOF >/etc/nginx/sites-available/$URL
+server {
+        listen 80;
+        listen [::]:80;
+
+        root /var/www/$URL/public;
+        index index.php index.html index.htm index.nginx-debian.html;
+
+        server_name $URL;
+
+        location / {
+                try_files \$uri \$uri/ =404;
+        }
+        
+        location ~ \.php$ {
+                include snippets/fastcgi-php.conf;
+                fastcgi_pass unix:/run/php/php7.0-fpm.sock;
+        }
+
+        location ~ /\.ht {
+                deny all;
+        }
+}
+EOF
+
+#symlink the files
+ln -s /etc/nginx/sites-available/$URL /etc/nginx/sites-enabled/
+
+#restart nginx
+sudo systemctl restart nginx
+
+#install composer
+#Usually composer is installed on host computer when using Vagrant
+#cd /tmp
+#curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+
+#create user and DB if needed
+mysql -uroot -e "CREATE DATABASE $DBNAME;"
+mysql -uroot -e "CREATE USER '$USER'@'localhost' IDENTIFIED BY '$PASSWORD';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$USER'@'localhost';"
+
+
+
